624913ae4280df33aadb755604b8ac60d557e7fa
Ariel Zingman <arielzingman@Ariels-MacBook-Pro.local>
2020-05-22 20:48:54 +0300

    starteeeen bones for DNode

---
+++ include/DNode.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class DNode
+{
+private:
+    std::vector <bool> m_legs;
+    std::vector <DNode *> m_neighbours;
+
+    sf::Vector2f m_loc;
+
+    bool m_on = false;
+
+
+public:
+
+    void draw(sf::RenderWindow&) const;
+    bool contains() const;
+    
+    void shiftL();
+    void shiftR();
+
+
+    DNode(std::vector<bool>, sf::Vector2f);
+    ~DNode();
+};
+
+DNode::DNode(std::vector<bool> legs, sf::Vector2f loc)
+    :m_legs (legs), m_loc (loc)
+{
+}
+
+DNode::~DNode()
+{
+}
---
+++ src/DNode.cpp
@@ -0,0 +1,22 @@
+#include <DNode.h>
+
+void DNode::draw(sf::RenderWindow& win) const
+{
+    sf::CircleShape circle;
+
+    circle.setRadius(1);
+    circle.setOrigin(m_loc);
+
+    if(m_on)
+        circle.setFillColor(sf::Color::White);
+    else
+        circle.setFillColor(sf::Color::Blue);
+    
+    win.draw(circle);
+
+    sf::RectangleShape edge;
+
+
+    
+
+}
